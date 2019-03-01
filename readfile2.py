23efbec9b19486126733739ade494d5866606c8c
88matthew88 <mmcfarlane1995@gmail.com>
2019-03-01 05:40:58 -0500

    minor adjustments

--- readfile2.py
+++ readfile2.py
@@ -2,9 +2,6 @@
 import csv
 import mysql.connector
 
-# initialize titles and rows list
-#fields = []
-#rows = []
 
 #csv file name
 filename = "Test-file.csv"
@@ -21,7 +18,8 @@ mycursor = mydb.cursor()
 #mycursor.execute("CREATE DATABASE IF NOT EXISTS mydatabase")
 
 # create student table
-mycursor.execute("CREATE TABLE IF NOT EXISTS students (name VARCHAR(50), Year VARCHAR(50), Dorm VARCHAR(50), Room VARCHAR(50), GPA VARCHAR(50))")
+mycursor.execute("CREATE TABLE IF NOT EXISTS students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), Year VARCHAR(50), Dorm VARCHAR(50), Room VARCHAR(50), GPA VARCHAR(50))")
+
 
 
 # reading csv file
@@ -29,19 +27,24 @@ with open(filename, "r") as csv_file:
     #creating a csv reader object
     csv_reader = csv.reader(csv_file)
 
-    # get field names
-    # fields = csvreader.next()
+    # A counter that will keep track of which line the program is on
+    line_count=0
 
     # A for loop to traverse the lines in  the CSV file
     for row in csv_reader:
-        print(row[0] + " was in the year " + row[1] + " lived in " +  row[2] + " and graduated with a GPA of " + row[3])
-        #mycursor.executemany('INSERT into %s VALUES(%s, %s, %s, %s, %s)'%students, row)
-        
-        sql = "INSERT INTO students (name, year, dorm,room,GPA)VALUES (%s, %s, %s, %s, %s)"
-        val = (row[0], row[1], row[2], row[3], row[4] )
-        mycursor.execute(sql,val)
-        mydb.commit()
-        
+        if line_count==0:
+            print(row[0] + " was in the year " + row[1] + " lived in " +  row[2] + " and graduated with a GPA of " + row[3])
+            line_count +=1
+
+        else:
+            sql = "INSERT INTO students (name, year, dorm,room,GPA)VALUES (%s, %s, %s, %s, %s)"
+            val = (row[0], row[1], row[2], row[3], row[4] )
+            mycursor.execute(sql,val)
+            mydb.commit()
+
+#sql= "DROP TABLE students"
+#mycursor.execute(sql)
+
 mycursor.execute("SELECT * FROM students")
 myresult = mycursor.fetchall()
 
